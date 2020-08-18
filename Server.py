4823a3e63bf2548ffd916bee63a36afc14cfe24f
hettysymes <hetty.symes@virginmedia.com>
2020-08-18 17:15:45 +0100

    Modified Server program so that messages are sent between the server and clients as objects rather than strings
    Messages are sent as Msg objects consisting of a sender, data, and a receiver. If the receiver is None, it is assumed the intended receiver is the server. Otherwise, the server redirects the transmission to the intended client.

--- Server.py
+++ Server.py
@@ -1,5 +1,18 @@
 import socket
 import threading
+import pickle
+from enum import Enum, auto
+
+class Msg:
+
+    def __init__(self, sender, data=None, receiver=None):
+        self.sender = sender
+        self.data = data
+        self.receiver = receiver
+
+class Cmd(Enum):
+    ADD = auto()
+    REM = auto()
 
 def client(username):
     host = socket.gethostname()
@@ -8,22 +21,22 @@ def client(username):
     s = socket.socket()
     s.connect((host, port))
 
-    s.send((username+"-").encode("utf-8"))
-    data = s.recv(1024).decode('utf-8')
+    s.send(pickle.dumps(Msg(username, Cmd.ADD)))
+    data = pickle.loads(s.recv(1024)).data
     print(data)
 
     print("Waiting for other player...")
-    opp = s.recv(1024).decode('utf-8')
+    opp = pickle.loads(s.recv(1024)).data
     print("Opponent: ", opp)
 
     if username<opp:
-        data = s.recv(1024).decode('utf-8')
+        data = pickle.loads(s.recv(1024)).data
         print("Opponent move: ", data)
 
     while 1:
         move = input("Move: ")
-        s.send((username+"-"+move).encode("utf-8"))
-        data = s.recv(1024).decode('utf-8')
+        s.send(pickle.dumps(Msg(username, move, opp)))
+        data = pickle.loads(s.recv(1024)).data
         print("Opponent move: ", data)
 
     s.close()
@@ -47,36 +60,30 @@ def makeConnection():
     global onlineUsers
     notPlaying = []
     for username, status in onlineUsers.items():
-        if status[1] == False: notPlaying.append(username)
-    print(notPlaying)
+        if not status[1]: notPlaying.append(username)
     while len(notPlaying) > 1:
         u1 = notPlaying.pop()
         u2 = notPlaying.pop()
-        onlineUsers[u1][1] = u2
-        onlineUsers[u2][1] = u1
-        onlineUsers[u1][0].send(u2.encode("utf-8"))
-        onlineUsers[u2][0].send(u1.encode("utf-8"))
+        onlineUsers[u1][1], onlineUsers[u2][1] = True, True
+        onlineUsers[u1][0].send(pickle.dumps(Msg("Server", u2)))
+        onlineUsers[u2][0].send(pickle.dumps(Msg("Server", u1)))
 
 def handle_client(c):
     while True:
-        data = c.recv(1024).decode("utf-8")
-        if not data:
+        recvMsg = c.recv(1024)
+        msg = pickle.loads(recvMsg)
+        if not msg:
             break
-        username, info = data.split("-")
         global onlineUsers
-        if username not in onlineUsers:
-            onlineUsers[username] = [c, False]
-            msg = "Added as a current online user"
-            c.send(msg.encode("utf-8"))
+        if msg.receiver != None:
+            onlineUsers[msg.receiver][0].send(recvMsg)
+        elif msg.data == Cmd.ADD:
+            onlineUsers[msg.sender] = [c, False]
+            c.send(pickle.dumps(Msg("Server", "Successfully added to current online players")))
             makeConnection()
-        elif onlineUsers[username][1] == False:
-            del onlineUsers[username]
-            msg = "Removed from current online users"
-            c.send(msg.encode("utf-8"))
-        else:
-            opp = onlineUsers[username][1]
-            onlineUsers[opp][0].send(info.encode("utf-8"))
-        
+        elif msg.data == Cmd.REM:
+            del onlineUsers[msg.sender]
+            c.send(pickle.dumps(Msg("Server", "Successfully deleted from current online players")))        
 
 inp = input("Client or server (c/s)? ")
 if inp == "s":
