f9f264ff25c4962c1b844c10d5dd4b788fb97a62
hettysymes <hetty.symes@virginmedia.com>
2020-12-18 15:14:12 +0000

    Modified AI program to provide level 2 difficulty on top of 0 and 1
    The getValue function was modified to improve the AI algorithm so that obvious moves to play weren't missed. This involved also creating two new functions getNumberOfCaptureLines and getNumberOfWinOpportunites to help calculate the value of a game state. The minimax function was edited so that only the game states with the top 3 values would be investigated further from each node. This was to increase the speed of the AI.

--- Ai.py
+++ Ai.py
@@ -45,16 +45,34 @@ def getNumberOfLines(board, lengths, player):
                         totals[i] += 1
     return totals
 
-# Returns the value of a game state given the board and captures.
-def getValue(board, captures):
-    p1lines = getNumberOfLines(board, [1, 2, 3, 4], Game.P1)
-    p2lines = getNumberOfLines(board, [1, 2, 3, 4], Game.P2)
-    val = 300*len(captures[Game.P1]) - 300*len(captures[Game.P2])
-    val += 10*p1lines[0] - 10*p2lines[0]
-    val += 20*p1lines[1] - 20*p2lines[1]
-    val += 50*p1lines[2] - 50*p2lines[2]
-    val += 999999*p1lines[3] - 999999*p2lines[3]
-    return val
+def getNumberOfCaptureLines(board, player):
+    total = 0
+    opp = Game.P1 if player == Game.P2 else Game.P2
+    boardsize = len(board)
+    for row in range(boardsize):
+        for col in range(boardsize):
+            if board[row][col] == player and Game.inRow(board, row, col, [opp, opp, Game.EMPTY]):
+                total += 1
+            elif board[row][col] == Game.EMPTY and Game.inRow(board, row, col, [opp, opp, player]):
+                total += 1
+    return total
+
+def getNumberOfWinOpportunities(board, captures, player, numberOfCaptureLines):
+    winOpportunities = 0
+    if numberOfCaptureLines + len(captures[player]) >= 5:
+        winOpportunities += len(captures[player]) - numberOfCaptureLines
+    for row in range(len(board)):
+        for col in range(len(board)):
+            if board[row][col] not in [player, Game.EMPTY]:
+                continue
+            validProducts = Game.getValidProducts([(0,1), (1,1), (1,0), (1,-1)], 4, row, col, len(board))
+            for rc in validProducts:
+                pieces = [board[row+i*rc[0]][col+i*rc[1]] for i in range(1, 5)]
+                if board[row][col] == player and (pieces.count(player)==3 and pieces.count(Game.EMPTY)==1):
+                    winOpportunities += 1
+                elif board[row][col] == Game.EMPTY and (pieces.count(player)==4):
+                    winOpportunities += 1
+    return winOpportunities
 
 # Returns the coordinates on the board which are next to an existing piece on the board.
 def getNextTo(board):
@@ -80,7 +98,22 @@ def pickRandomMove(board):
                 emptyCoords.append((row, col))
     return random.choice(emptyCoords)
 
-def canMakeImmediateMove(board, captures, player):
+# Returns the value of a game state given the board and captures.
+def getValue(board, captures):
+    p1lines = getNumberOfLines(board, [1, 2, 3, 4], Game.P1)
+    p2lines = getNumberOfLines(board, [1, 2, 3, 4], Game.P2)
+    p1CaptureLines = getNumberOfCaptureLines(board, Game.P1)
+    p2CaptureLines = getNumberOfCaptureLines(board, Game.P2)
+    val = 30000*(len(captures[Game.P1]) - len(captures[Game.P2]))
+    val += 10*(p1lines[0] - p2lines[0])
+    val += 20*(p1lines[1] - p2lines[1])
+    val += 50*(p1lines[2] - p2lines[2])
+    val += 999999999999*2*(p1lines[3] - p2lines[3])
+    val += 10000*(p1CaptureLines - p2CaptureLines)
+    val += 999999999999*(getNumberOfWinOpportunities(board, captures, Game.P1, p1CaptureLines) - getNumberOfWinOpportunities(board, captures, Game.P2, p2CaptureLines))
+    return val
+
+def getImmediateMove(board, captures, player):
     move = [None, 0]
     opp = Game.P1 if player == Game.P2 else Game.P2
     for row in range(len(board)):
@@ -99,14 +132,15 @@ def canMakeImmediateMove(board, captures, player):
                 move = [(row, col), 3]
             elif len(tempCaptures[opp]) > len(captures[opp]) and move[1] < 2:
                 move = [(row, col), 2]
-            elif Game.inRow(board, row, col, [opp, opp, Game.EMPTY]) and move[1] < 1:
-                move = [(row, col), 1]
+            elif Game.inRow(board, row, col, [opp, opp, Game.EMPTY]) and move[1] < 2:
+                move = [(row, col), 2]
     if move[0] != None:
         return True, move[0]
     return False, ()
 
 # Performs the minimax algorithm to a specified depth, and returns the calculated move for the AI.
-def minimax(board, captures, player, node, depth, alpha=(-inf,), beta=(inf,)):
+def minimax(board, captures, player, node, depth, alpha=(-inf,), beta=(inf,), movesToAnalyse=3):
+
     winner = Game.getWinner(board, captures)
     if winner != Game.ONGOING:
         if winner == Game.P1:
@@ -123,21 +157,20 @@ def minimax(board, captures, player, node, depth, alpha=(-inf,), beta=(inf,)):
     
     nextTo = getNextTo(board)
     if len(nextTo) == 0:
-        success = False
-        for row in range(len(board)):
-            for col in range(len(board)):
-                if board[row][col] == Game.EMPTY:
-                    nextTo.append((row, col))
-                    success = True
-                    break
-            if success: break
+        nextTo.append(pickRandomMove(board))
     for row, col in nextTo:
         node.addChild(row, col)
 
     if player == Game.P1:
         maxEval = (-inf, node.children[0].row, node.children[0].col)
+        childrenValues = []
         for child in node.children:
             tempBoard, tempCaptures, tempPlayer = Game.newState(board, captures, player, child.row, child.col)
+            childrenValues.append([getValue(tempBoard, tempCaptures), tempBoard, tempCaptures, child])
+        childrenValues.sort(key=itemgetter(0))
+        for _ in range(movesToAnalyse):
+            if len(childrenValues) == 0: break
+            value, tempBoard, tempCaptures, child = childrenValues.pop()
             eval = minimax(tempBoard, tempCaptures, tempPlayer, child, depth-1, alpha, beta)
             maxEval = max([maxEval, eval], key=itemgetter(0))
             alpha = max([alpha, eval], key=itemgetter(0))
@@ -146,8 +179,14 @@ def minimax(board, captures, player, node, depth, alpha=(-inf,), beta=(inf,)):
         return (maxEval[0], node.row, node.col) if not node.root else maxEval
     else:
         minEval = (inf, node.children[0].row, node.children[0].col)
+        childrenValues = []
         for child in node.children:
             tempBoard, tempCaptures, tempPlayer = Game.newState(board, captures, player, child.row, child.col)
+            childrenValues.append([getValue(tempBoard, tempCaptures), tempBoard, tempCaptures, child])
+        childrenValues.sort(key=itemgetter(0), reverse=True)
+        for _ in range(movesToAnalyse):
+            if len(childrenValues) == 0: break
+            value, tempBoard, tempCaptures, child = childrenValues.pop()
             eval = minimax(tempBoard, tempCaptures, tempPlayer, child, depth-1, alpha, beta)
             minEval = min([minEval, eval], key=itemgetter(0))
             beta = min([beta, eval], key=itemgetter(0))
@@ -156,11 +195,11 @@ def minimax(board, captures, player, node, depth, alpha=(-inf,), beta=(inf,)):
         return (minEval[0], node.row, node.col) if not node.root else minEval
 
 # Given a board, captures, and player the play function gets a move from the minimax algorithm for the AI to play and returns it.
-def play(board, captures, player, difficulty=1):
+def play(board, captures, player, difficulty=2):
     if difficulty == 0:
         return pickRandomMove(board)
     elif difficulty == 1:
-        canMake, move = canMakeImmediateMove(board, captures, player)
+        canMake, move = getImmediateMove(board, captures, player)
         if canMake:
             return move
         else:
